bc2389d33f5fcaf6b4ceb1af4c0397a1c5ba5198
parksr89 <54193969+parksr89@users.noreply.github.com>
2023-10-18 13:14:58 -0700

    update index.js

--- build/index.js
+++ build/index.js
@@ -1,9 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// example 1
+//import * as crypto from "crypto";
+// example 2 -> tsconfig
+const crypto_1 = __importDefault(require("crypto"));
 class Block {
-    constructor(data) {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
         this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
-    static hello() {
-        return "hi";
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
     }
 }
-export {};
+class BlockChain {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevHash() {
+        if (this.blocks.length === 0)
+            return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    getBlocks() {
+        return [...this.blocks];
+    }
+}
+const blockchain = new BlockChain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+console.log(blockchain.getBlocks());
